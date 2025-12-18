0eba58d6c6e470ef404ee162557515cbff2baf03
BITS-Rohit <rohitguptaradheradhe123@gmail.com>
2025-12-18 14:00:37 +0530

    Code

--- src/Code/Lcode.java
+++ src/Code/Lcode.java
@@ -10103,6 +10103,24 @@ public class Lcode {
         return ans;
     }
 
+    public long maxProfit(int[] prices, int[] strategy, int k) {
+        int n = prices.length;
+        long[] profitSum = new long[n + 1];
+        long[] priceSum = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            profitSum[i + 1] = profitSum[i] + (long) prices[i] * strategy[i];
+            priceSum[i + 1] = priceSum[i] + prices[i];
+        }
+        long res = profitSum[n];
+        for (int i = k - 1; i < n; i++) {
+            long leftProfit = profitSum[i - k + 1];
+            long rightProfit = profitSum[n] - profitSum[i + 1];
+            long changeProfit = priceSum[i + 1] - priceSum[i - k / 2 + 1];
+            res = Math.max(res, leftProfit + changeProfit + rightProfit);
+        }
+        return res;
+    }
+
     /// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public static void main(String[] args) {
         Lcode l = new Lcode();
