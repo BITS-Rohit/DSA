9760ca4eee9e7ac0061c3aba54a576824ea7f946
BITS-Rohit <rohitguptaradheradhe123@gmail.com>
2025-12-30 15:36:25 +0530

    Code

--- src/Code/Lcode.java
+++ src/Code/Lcode.java
@@ -10596,6 +10596,49 @@ public class Lcode {
         return false;
     }
 
+    public int numMagicSquaresInside(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+
+        for (int i = 0; i <= m - 3; i++) {
+            for (int j = 0; j <= n - 3; j++) {
+                if (isMagic(grid, i, j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private boolean isMagic(int[][] grid, int r, int c) {
+        boolean[] seen = new boolean[10];
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                int val = grid[i][j];
+                if (val < 1 || val > 9 || seen[val]) return false;
+                seen[val] = true;
+            }
+        }
+
+        int row1 = grid[r][c] + grid[r][c+1] + grid[r][c+2];
+        int row2 = grid[r+1][c] + grid[r+1][c+1] + grid[r+1][c+2];
+        int row3 = grid[r+2][c] + grid[r+2][c+1] + grid[r+2][c+2];
+
+        if (row1 != 15 || row2 != 15 || row3 != 15) return false;
+
+        int col1 = grid[r][c] + grid[r+1][c] + grid[r+2][c];
+        int col2 = grid[r][c+1] + grid[r+1][c+1] + grid[r+2][c+1];
+        int col3 = grid[r][c+2] + grid[r+1][c+2] + grid[r+2][c+2];
+
+        if (col1 != 15 || col2 != 15 || col3 != 15) return false;
+
+        int d1 = grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2];
+        int d2 = grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c];
+
+        return d1 == 15 && d2 == 15;
+    }
+
     /// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public static void main(String[] args) {
         Lcode l = new Lcode();
