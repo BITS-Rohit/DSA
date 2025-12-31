10795d48472022c57a48df5b1c8ee1f8982779b6
BITS-Rohit <rohitguptaradheradhe123@gmail.com>
2025-12-31 23:25:07 +0530

    Code

--- src/Code/Lcode.java
+++ src/Code/Lcode.java
@@ -10639,6 +10639,45 @@ public class Lcode {
         return d1 == 15 && d2 == 15;
     }
 
+    int R, C;
+    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    public int latestDayToCross(int row, int col, int[][] cells) {
+        R = row; C = col;
+        int lo = 0, hi = cells.length, ans = 0;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (canCross(mid, cells)) {
+                ans = mid; lo = mid + 1;
+            } else hi = mid - 1;
+        }
+        return ans;
+    }
+
+    boolean canCross(int day, int[][] cells) {
+        boolean[][] water = new boolean[R][C];
+        for (int i = 0; i < day; i++)
+            water[cells[i][0]-1][cells[i][1]-1] = true;
+
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        boolean[][] vis = new boolean[R][C];
+        for (int c = 0; c < C; c++)
+            if (!water[0][c]) { q.add(new int[]{0,c}); vis[0][c] = true; }
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            if (cur[0] == R-1) return true;
+            for (int[] d : dirs) {
+                int nr = cur[0] + d[0], nc = cur[1] + d[1];
+                if (nr>=0 && nr<R && nc>=0 && nc<C && !water[nr][nc] && !vis[nr][nc]) {
+                    vis[nr][nc] = true;
+                    q.add(new int[]{nr,nc});
+                }
+            }
+        }
+        return false;
+    }
+
     /// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public static void main(String[] args) {
         Lcode l = new Lcode();
